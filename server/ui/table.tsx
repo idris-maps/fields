00af5db7abca5439084573f12145edc19f02a9b3
Anders <idris.maps@gmx.ch>
2022-02-13 23:41:10 +0100

    Add table component

---
+++ server/ui/table.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "../deps.ts";
+import "../types.d.ts";
+import type { Field } from "../deps.ts";
+
+const Head = ({ fields }: { fields: Field[] }) => (
+  <thead>
+    <tr>
+      {fields.map((d) => <th>${d.label || d.property || ""}</th>)}
+    </tr>
+  </thead>
+);
+
+const Row = ({ fields, data }: { fields: Field[]; data: any }) => (
+  <tr>
+    {fields.map(({ property, type }) => (
+      <td>{data[property] || (type === "checkbox" ? "false" : "")}</td>
+    ))}
+  </tr>
+);
+
+const Table = ({ fields, data }: { fields: Field[]; data: any[] }) => (
+  <table>
+    <Head fields={fields} />
+    <tbody>
+      {data.map((d) => <Row fields={fields} data={d} />)}
+    </tbody>
+  </table>
+);
+
+export default Table;
