cdb692d3c8e5b94ed907d4b5f48bb84238e7d850
Anders <idris.maps@gmx.ch>
2022-04-24 22:34:16 +0200

    layout viewport

--- server/ui/layout.tsx
+++ server/ui/layout.tsx
@@ -27,6 +27,7 @@ export default ({
   <html>
     <head>
       <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <Each<string>
         data={[...defaultCss, ...(css || [])]}
         render={(d) => <link rel="stylesheet" href={d} />}
